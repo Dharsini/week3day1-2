5336abbd967f160f14990a826e871de77cb9bf9e
Dharsini <dharsinieee@gmail.com>
2021-12-11 16:49:27 -0800

    Add files via upload

---
+++ Axissbank.java
@@ -0,0 +1,21 @@
+package org.bank;
+
+public class Axissbank extends BankInfo{
+	
+	public void deposit() {
+		System.out.println("10% interest");
+		
+		
+		
+	}
+
+	public static void main(String[] args) {
+		
+		Axissbank ax = new Axissbank();
+		
+		ax.deposit();
+		
+ 
+	}
+
+}
