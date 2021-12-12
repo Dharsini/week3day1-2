98c6438ac55b24aa88fa26fd88f956d04df03021
Dharsini <dharsinieee@gmail.com>
2021-12-11 21:47:52 -0800

    Add files via upload

---
+++ College.java
@@ -0,0 +1,20 @@
+package org.college;
+
+public class College {
+	
+	public void collegeName() {
+		
+		System.out.println("NEC");
+	}
+	
+	public void collegeCode() {
+		
+		System.out.println("4902");
+	}
+	
+	public void collegeRank() {
+		
+		System.out.println("47");
+	}
+
+}
